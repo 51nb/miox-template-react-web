269f60c96f9e85f97b3244bcc3cfc1e6f545b179
shenyunjie <shenyunjie@u51.com>
2017-11-01 18:43:44 +0800

    update

---
+++ fe.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "name": "fe-template-react-web",
+  "description": "创建React项目模板",
+  "version": "1.0",
+  "engine": "2.0",
+  "author": "沈赟杰",
+  "email": "shenyunjie@u51.com",
+  "extension": {}
+};
