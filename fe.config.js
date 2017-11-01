c3f648d812b36a929ee98d8fffe8b6f8c098600a
shenyunjie <shenyunjie@u51.com>
2017-11-01 18:52:07 +0800

    update

--- fe.config.js
+++ fe.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "name": "miox-template-react-web",
-  "description": "创建React项目模板",
+  "description": "创建Miox@5.x + React@16.x项目模板(非SSR)",
   "version": "1.0",
   "engine": "2.0",
   "author": "沈赟杰",
